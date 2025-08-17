d3076793c538f5324c3126145f45041a0af94a07
Raiyan <raiyan.fzs845@gmail.com>
2025-08-18 02:43:41 +0600

    dekhi

--- backend/src/main/java/com/mindcare/connect/MindCareConnectApplication.java
+++ backend/src/main/java/com/mindcare/connect/MindCareConnectApplication.java
@@ -2,21 +2,11 @@ package com.mindcare.connect;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableJpaAuditing
-@EnableJpaRepositories
-@EnableTransactionManagement
 public class MindCareConnectApplication {
     
     public static void main(String[] args) {
-        // Set system properties for better Docker compatibility
-        System.setProperty("spring.jpa.open-in-view", "false");
-        System.setProperty("server.address", "0.0.0.0");
-        
         SpringApplication.run(MindCareConnectApplication.class, args);
     }
 }
